3227d8bdbf850df8bc393649afafa921a822ca8d
Prashanth <88532366+geekPachi@users.noreply.github.com>
2023-06-09 12:52:42 +0530

    Update dataprep.py

--- Regresion_Salary/dataprep.py
+++ Regresion_Salary/dataprep.py
@@ -1,1 +1,19 @@
 
+import pandas as pd
+
+import numpy as np
+
+df = pd.read_csv('Salary_Data.csv')
+
+x = df.iloc[:,:-1].values
+y = df.iloc[:,-1].values
+
+y = y.reshape(len(y),1)
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+
+x = sc.fit_transform(x)
+y = sc.fit_transform(y)
+
